29fc2568f84a282ba267af71088870337c316854
Steven Notovitz <sjn39@cornell.edu>
2023-03-23 14:52:34 -0400

    Add producereducer

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import produceReducer from "./produce";
 
 const rootReducer = combineReducers({
-
+  produce: produceReducer
 });
 
 let enhancer;
@@ -12,7 +13,6 @@ if (process.env.NODE_ENV !== "production") {
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(logger));
 }
-
 
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
