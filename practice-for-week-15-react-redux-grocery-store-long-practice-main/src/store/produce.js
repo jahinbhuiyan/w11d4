438bc273cf020cba9b202a5085350ea84f4f778c
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-23 15:02:38 -0400

    phase 2

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -1,6 +1,15 @@
+import * as produceData from '../mockData/produce.json'
+
 export default function produceReducer(state={}, action) {
   switch (action.type) {
     default:
       return state
   }
+}
+
+const POPULATE = "produce/populate"
+
+export function populateProduce(){
+
+  return {type: POPULATE, produce: produceData}
 }
