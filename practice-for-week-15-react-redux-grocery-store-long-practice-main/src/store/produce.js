3f50987e56094243c64481fe4d085a6b1767a9fd
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-23 15:15:37 -0400

    phase 2 nearly done

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -2,6 +2,12 @@ import * as produceData from '../mockData/produce.json'
 
 export default function produceReducer(state={}, action) {
   switch (action.type) {
+    case POPULATE:
+      let newState = {}
+      for(let produce of action.produce){
+        newState[produce.id] = produce
+      }
+      return newState
     default:
       return state
   }
