117ad1657e82355cc909ad7f2712bfea8e32328b
Steven Notovitz <sjn39@cornell.edu>
2023-03-23 15:44:32 -0400

    Add dispatch

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -1,21 +1,20 @@
-import * as produceData from '../mockData/produce.json'
+import produceData from '../mockData/produce.json';
 
-export default function produceReducer(state={}, action) {
-  switch (action.type) {
-    case POPULATE:
-      let newState = {}
-      for(let produce of action.produce){
-        newState[produce.id] = produce
-      }
-      return newState
-    default:
-      return state
-  }
+const POPULATE = "produce/POPULATE";
+
+export function populateProduce() {
+  return { type: POPULATE, produce: produceData };
 }
 
-const POPULATE = "produce/populate"
-
-export function populateProduce(){
-
-  return {type: POPULATE, produce: produceData}
+export default function produceReducer(state = {}, action) {
+  switch (action.type) {
+    case POPULATE:
+      const newState = {};
+      for (let produce of action.produce){
+        newState[produce.id] = produce;
+      };
+      return newState;
+    default:
+      return state;
+  }
 }
