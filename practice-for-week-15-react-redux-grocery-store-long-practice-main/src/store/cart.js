def8a94c4270fdcae8a47f38a640579d35726205
Steven Notovitz <sjn39@cornell.edu>
2023-03-23 16:26:18 -0400

    Update cart.js

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/cart.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/cart.js
@@ -1,8 +1,26 @@
+import produceData from '../mockData/produce.json';
 
-export default function cartReducer(state = {}, action) {
-    switch(action.type){
-        default: 
-        return state
-    }
+function arrToObj(arr) {
+  const obj = {};
+  for (let ele of arr){
+    obj[ele.id] = ele;
+  };
+  return obj;
 }
 
+const ADD = "cart/ADD";
+
+export function addToCart(id) {
+  let data = arrToObj(produceData);
+  return { type: ADD, id: data[id] };
+}
+
+export default function cartReducer(state = {}, action) {
+  switch(action.type) {
+    case ADD:
+      addToCart()
+      return 
+    default: 
+      return state;
+  }
+}
